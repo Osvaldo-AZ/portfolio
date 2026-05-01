a69e382551145e8cdc952303584297eb64ec096e
oriver11 <oriver11@asu.edu>
2026-05-01 01:10:14 -0700

    Add Tailwind CSS and font support to the project
    - Updated astro.config.mjs to include Tailwind CSS and font providers.
    - Modified package.json and package-lock.json to add dependencies for Tailwind CSS and font packages.
    - Created globals.d.ts for module declarations for font sources.
    - Added global.css for Tailwind CSS styles and font settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Inter Variable',
+      cssVariable: '--font-inter',
+    }, {
+      provider: fontProviders.fontsource(),
+      name: 'IBM Plex Mono',
+      cssVariable: '--font-ibm-plex-mono',
+    }]
+});
